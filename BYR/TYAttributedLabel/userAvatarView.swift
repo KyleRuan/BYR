81fd945b98f369c6ff7d0b358e1e05fe6b14c125
kyleruan <kyleruan@163.com>
2016-05-23 00:57:23 +0800

    fix

--- BYR/TYAttributedLabel/userAvatarView.swift
+++ BYR/TYAttributedLabel/userAvatarView.swift
@@ -9,36 +9,101 @@
 import UIKit
 
 class userAvatarView: UIView {
+        
+        //    let screenW = UIScreen.mainScreen().bounds.width
+        let screenH = UIScreen.mainScreen().bounds.height
+        //
+        
+        var ImageBG:UIImage!{
+            didSet{
+            
+                let scaleImgae = UIImage.scaleTosizeImage(ImageBG, size: CGSizeMake(self.bounds.width,self.bounds.height))
+              ImageBG = scaleImgae
+                
+                setNeedsDisplay()
+            }
+        }
+    var avatarRadius:CGFloat = 40{
+        didSet{
+            addAvatar()
+        }
+    }
+        var rectColor = UIColor.blueColor()
+    var borderWidth:CGFloat=2 {
+        didSet{
+            addAvatar()
+        }
+    }
+        var avatar:UIImage!{
+            didSet{
+                addAvatar()
+            }
+        }
+        
+        override  init(frame: CGRect) {
+            super.init(frame: frame)
+            addAvatar()
+            
+//            self.bounds.c
+            
+        }
+        
+    
+        //
+    convenience init(frame:CGRect,backgroundImage:UIImage,avatarImage:UIImage,avatarRadius:CGFloat = 40) {
+            //      let screenW = UIScreen.mainScreen().bounds.width
+            self.init(frame: frame)
+            ImageBG = UIImage.scaleTosizeImage(backgroundImage, size: CGSizeMake(self.bounds.width,self.bounds.height))
+            self.avatarRadius = avatarRadius
+            
+            avatar = avatarImage
+            addAvatar()
+            
+            
+        }
+        
+        
+        
+        
+        private func addAvatar() {
+            
+            
+            if avatar == nil {
+                avatar = UIImage(named: "face_default_m")
+            }
+            
+            let imageViewSizeX = UIScreen.mainScreen().bounds.midX-(avatarRadius+borderWidth)
+            let imageViewSizeY = self.bounds.maxY
+            let imageView = UIImageView(frame: CGRectMake(imageViewSizeX, imageViewSizeY-2*(avatarRadius+borderWidth),2*(avatarRadius+borderWidth), 2*(avatarRadius+borderWidth)))
+            let scaleImgae = UIImage.scaleTosizeImage(avatar, size: CGSizeMake(avatarRadius*2,avatarRadius*2))
+            let avatars = UIImage.borderCircleImage(scaleImgae, borderWidth: borderWidth, borderColor:rectColor,size: CGRectMake(0,0,avatarRadius*2,avatarRadius*2))
+            imageView.image = avatars
+            self.clipsToBounds = false
+            self.addSubview(imageView)
+            
+            
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+        }
+        
+        
+        // Only override drawRect: if you perform custom drawing.
+        // An empty implementation adversely affects performance during animation.
+        override func drawRect(rect: CGRect) {
+            // Drawing code
+            if ImageBG == nil {
+                // backgroundView
+                ImageBG = UIImage(named: "userInfBackground")
+            }
+            
+            
+            ImageBG?.drawInRect(self.bounds)
+        }
+        
+        
+    }
+    
 
-    var avatar:String!
-    
-    
-   
-    
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        avatar = ""
-    }
-    
-    convenience init(avatar:String) {
-        self.init(frame:CGRectZero)
-        self.avatar = avatar
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-   
-    
 
-    
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
-
-}
---
+++ BYR/UIImageExtension.swift
@@ -0,0 +1,71 @@
+//
+//  UIImageExtension.swift
+//  BYR
+//
+//  Created by Jason on 5/21/16.
+//  Copyright © 2016 KYLERUAN. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIImage {
+    static func borderCircleImage(image:UIImage,borderWidth:CGFloat,borderColor:UIColor,size:CGRect) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(size.width, size.height), false, 0)
+        
+        // avatar居中，直径为80的园
+        // 要求这个图片是  Width= Height
+        let imageWH = image.size.width ;
+        
+        let ovalWH = imageWH+borderWidth*2
+        
+        UIGraphicsBeginImageContext(CGSizeMake(ovalWH, ovalWH))
+        
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(ovalWH, ovalWH), false, 0)
+        
+        //        // 画头像的大圆
+        let path = UIBezierPath(ovalInRect: CGRectMake(0, 0, ovalWH, ovalWH))
+        borderColor.set()
+        path.fill()
+        
+        //        //
+        // 3.设置裁剪区域
+        let clipPath = UIBezierPath(ovalInRect: CGRectMake(borderWidth, borderWidth, imageWH, imageWH))
+        clipPath.addClip()
+        
+        
+        
+        
+        image.drawAtPoint(CGPointMake(borderWidth, borderWidth))
+        //        //
+        //        //        // 4.绘制图片
+        //        //        [image drawAtPoint:CGPointMake(border, border)];
+        //        //           image?.drawAtPoint(CGPointMake(midX-avatarRadius, maxY))
+        //
+        //        self.drawInRect(avatarFrame)
+        //        //
+        //        //        // 5.获取图片
+        let clipImage = UIGraphicsGetImageFromCurrentImageContext();
+        //        //
+        //        // 6.关闭上下文
+        UIGraphicsEndImageContext();
+        //        //
+        //
+        return clipImage
+        //
+    }
+    
+    static func scaleTosizeImage(image:UIImage,size:CGSize)->UIImage {
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        
+        image.drawInRect(CGRectMake(0, 0, size.width, size.height))
+        
+        
+        let scaled = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaled
+    }
+    
+}
